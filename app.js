b921e1a108ab642e1d5205ddca029dbf7b94c6e3
TusharJade <79801546+TusharJade@users.noreply.github.com>
2021-10-03 21:22:24 +0530

    referance given

---
+++ app.js
@@ -0,0 +1,6 @@
+var initialPrice = document.querySelector("#initial-price");
+var quantity = document.querySelector("#quantity");
+var currentPrice = document.querySelector("#current-price");
+var btn = document.querySelector("#btn");
+var output = document.querySelector("#output");
+
